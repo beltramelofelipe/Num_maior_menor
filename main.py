52e222b0039c51075edad1053b987fe5f07166be
Felipe Beltramelo <beltramelofelipe@gmail.com>
2021-03-09 01:42:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+num =  int(input('Digite um numero: '))
+num1 = int(input('Digite um numero: '))
+num2 = int(input('Digite um numero: '))
+
+if(num > num1) and (num > num2):
+  print('O numero {} é o número maior'.format(num))
+
+if(num1 > num) and (num1 > num2):
+  print('O numero {} é o número maior'.format(num1))
+
+if(num2 > num1) and (num2 > num1):
+  print('O numero {} é o número maior'.format(num2))
+
+if(num < num1) and (num < num2):
+  print('O menor é o numero {}'.format(num))
+
+if(num1 < num) and (num1 < num2):
+  print(' menor é o numero {}'.format(num1))
+
+if(num2 < num1) and (num2 < num1):
+  print(' menor é o numero {}'.format(num2))
+
+
